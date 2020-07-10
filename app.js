a4515fce4a25b31c91be85973615fef01f0889b0
sourav saha <souravsaha675@gmail.com>
2020-07-10 23:53:38 +0530

    changing app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://MongoUrL/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://MongoUrI/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const itemsSchema = {
   name:String
